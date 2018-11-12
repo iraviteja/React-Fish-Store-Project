c75f54341c4ee95032c0717b925be7a0b0d3afc9
iraviteja <iraviteja77@gmail.com>
2018-11-12 12:33:26 +0530

    Understanding state

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh see food market" />
         </div>
         <Order />
         <Inventory />
--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ class Header extends Component {
           day
         </h1>
         <h3 className="tagline">
-          <span>Fresh Daily</span>
+          <span>{this.props.tagline}</span>
         </h3>
       </header>
     );
