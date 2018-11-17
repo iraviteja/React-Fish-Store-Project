b0c59cc742fae7446ff9009368af055e177d1fbd
iraviteja <iraviteja77@gmail.com>
2018-11-17 17:05:03 +0530

    Understanding State

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,18 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 
 class App extends Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // 1. Take a copy of exiting state
+    const fishesCopy = { ...this.state.fishes };
+    // 2. Add our new fish to that fishes variable
+    fishesCopy[`fish${Date.now()}`] = fish;
+    // 3. Set the new fish object to state
+    this.setState({ fishes: fishesCopy });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +23,7 @@ class App extends Component {
           <Header tagline="Fresh see food market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
