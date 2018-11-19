4e79ef15772b231f029deede0ca027358d382139
iraviteja <iraviteja77@gmail.com>
2018-11-20 01:06:12 +0530

    Component Validation with PropTypes

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+
 import { formatPrice } from "./helpers";
 
 class Fish extends Component {
+  static propTypes = {
+    fishData: PropTypes.shape({
+      image: PropTypes.string,
+      name: PropTypes.string,
+      desc: PropTypes.string,
+      status: PropTypes.string,
+      price: PropTypes.number
+    }),
+    addToOrder: PropTypes.func
+  };
   handleClick = () => {
     this.props.addToOrder(this.props.index);
   };
   render() {
-    const { price, desc, status } = this.props.fishData;
+    const { name, price, desc, status, image } = this.props.fishData;
     const isAvailable = status === "available";
     return (
       <li className="menu-fish">
-        <img src={this.props.fishData.image} alt={this.props.fishData.name} />
+        <img src={image} alt={name} />
         <h4 className="fish-name">
-          {this.props.fishData.name}
+          {name}
           <span className="price">{formatPrice(price)}</span>
         </h4>
         <p>{desc}</p>
